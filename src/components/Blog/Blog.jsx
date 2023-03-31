26a8193a2f83b09d488ef46ec31b8b5b14b864a2
Partho Roy <roypartho772@gmail.com>
2023-03-31 19:47:55 +0600

    blog styles updated

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -4,7 +4,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBookmark } from '@fortawesome/free-solid-svg-icons'
 
 const Blog = (props) => {
-  console.log(props.blog);
   const {
     _id,
     blog_title,
@@ -15,7 +14,7 @@ const Blog = (props) => {
     author_name,
   } = props.blog;
   return (
-    <div className="px-8">
+    <div className="px-8 my-3">
       <div className="card w-full bg-base-100 shadow-md">
         <div className="p-4">
           <img className="w-full rounded-xl" src={blog_img} alt="" />
@@ -25,19 +24,19 @@ const Blog = (props) => {
             <div className="flex gap-3 items-center">
               <img className="w-16 rounded-full" src={author_img} alt="" />
               <div>
-                <h4>{author_name}</h4>
-                <p>{publish_date}</p>
+                <h4 className="font-bold text-xl">{author_name}</h4>
+                <p className="text-zinc-500-700">{publish_date}</p>
               </div>
             </div>
             <div className="flex items-center gap-3">
               <p>{read_time} min read</p>
-              <button>
+              <button className="">
                 <FontAwesomeIcon icon={faBookmark} />
               </button>
             </div>
           </div>
           <h2 className="card-title text-4xl my-4 font-semibold font-serif">{blog_title}</h2>
-          <p>#beginners #programming</p>
+          <p className="text-cyan-700">#beginners #programming</p>
           <div className="card-actions justify-start">
             <a className="text-blue-800 font-bold underline mt-4" href="/link">Mark as read</a>
           </div>
